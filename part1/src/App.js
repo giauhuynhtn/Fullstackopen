9cf9614b3e8fa8632217a6298d38d54ef61869d0
Giau Huynh <giau.huynh@tuni.fi>
2022-04-01 17:39:17 +0300

    1.12*

--- part1/src/App.js
+++ part1/src/App.js
@@ -261,81 +261,115 @@
 
 //--------Ex1.11---------
 
+// import {useState} from 'react'
+
+// const StatisticTable = ({value}) => {
+//   const {good, neutral, bad} = value
+//   let total = good + bad + neutral
+//   let average = (good + bad)/total
+//   let positive = good/total
+//   console.log(total)
+//   let result = <p>no feedback</p>
+//   if (total > 0) {result = (
+//       <table>
+//         <tbody>
+//           <tr>
+//             <td>good</td>
+//             <td>{good}</td>
+//           </tr>
+//           <tr>
+//             <td>neutral</td>
+//             <td>{neutral}</td>
+//           </tr>
+//           <tr>
+//             <td>bad</td>
+//             <td>{bad}</td>
+//           </tr>
+//           <tr>
+//             <td>all</td>
+//             <td>{total}</td>
+//           </tr>
+//           <tr>
+//             <td>average</td>
+//             <td>{average}</td>
+//           </tr>
+//           <tr>
+//             <td>positive</td>
+//             <td>{positive} %</td>
+//           </tr>
+//         </tbody>
+//       </table>
+//   )}
+  
+//   return result
+// }
+// const Button = ({text, onclick}) => <button onClick={onclick}>{text}</button>
+
+// const App = () => {
+ 
+//   const [good, setGood] = useState(0)
+//   const [neutral, setNeutral] = useState(0)
+//   const [bad, setBad] = useState(0)
+
+//   const goodHandler = () => {
+//     console.log('Customer chooses good')
+//     setGood(good + 1)
+//   }
+//   const neutralHandler = () => {
+//     console.log('Customer chooses neutral')
+//     setNeutral(neutral + 1)
+//   }
+//   const badHandler = () => {
+//     console.log('Customer chooses bad')
+//     setBad(bad + 1)
+//   }
+  
+//   let value = {good, neutral, bad}
+
+//   return (
+//     <div>
+//       <h1>give feedback</h1>
+//       <StatisticTable value ={value} />
+//       <p>
+//         <Button onclick={goodHandler} text='good'/>
+//         <Button onclick={neutralHandler} text='neutral'/>
+//         <Button onclick={badHandler} text='bad'/>
+//       </p>
+      
+//     </div>
+//   )
+// }
+
+// export default App
+
+// ------- 1.12 * ------------
 import {useState} from 'react'
 
-const StatisticTable = ({value}) => {
-  const {good, neutral, bad} = value
-  let total = good + bad + neutral
-  let average = (good + bad)/total
-  let positive = good/total
-  console.log(total)
-  let result = <p>no feedback</p>
-  if (total > 0) {result = (
-      <table>
-        <tbody>
-          <tr>
-            <td>good</td>
-            <td>{good}</td>
-          </tr>
-          <tr>
-            <td>neutral</td>
-            <td>{neutral}</td>
-          </tr>
-          <tr>
-            <td>bad</td>
-            <td>{bad}</td>
-          </tr>
-          <tr>
-            <td>all</td>
-            <td>{total}</td>
-          </tr>
-          <tr>
-            <td>average</td>
-            <td>{average}</td>
-          </tr>
-          <tr>
-            <td>positive</td>
-            <td>{positive} %</td>
-          </tr>
-        </tbody>
-      </table>
-  )}
-  
-  return result
-}
-const Button = ({text, onclick}) => <button onClick={onclick}>{text}</button>
+const getRandomInt = (max) => Math.floor(Math.random() * max)
 
 const App = () => {
- 
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
 
-  const goodHandler = () => {
-    console.log('Customer chooses good')
-    setGood(good + 1)
+  const selectHandle = () => {
+    let number = getRandomInt(anecdotes.length)
+    if (number === selected) {number = getRandomInt(anecdotes.length)}
+    else (setSelected(number))
   }
-  const neutralHandler = () => {
-    console.log('Customer chooses neutral')
-    setNeutral(neutral + 1)
-  }
-  const badHandler = () => {
-    console.log('Customer chooses bad')
-    setBad(bad + 1)
-  }
-  
-  let value = {good, neutral, bad}
-
   return (
     <div>
-      <h1>give feedback</h1>
-      <StatisticTable value ={value} />
-      <p>
-        <Button onclick={goodHandler} text='good'/>
-        <Button onclick={neutralHandler} text='neutral'/>
-        <Button onclick={badHandler} text='bad'/>
-      </p>
-      
+      {anecdotes[selected]} 
+      <br/>
+      <button onClick={selectHandle}>next anecdote</button>
     </div>
   )
 }
