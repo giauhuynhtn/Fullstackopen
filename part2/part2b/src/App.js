bc4cf6a65d4b96bc36f6a1fd06bcd15514ab8b01
Giau Huynh <giau.huynh@tuni.fi>
2022-04-06 14:12:04 +0300

    part2b 2.8 Phonebook step3

--- part2/part2b/src/App.js
+++ part2/part2b/src/App.js
@@ -2,19 +2,23 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(newName)
     const copy = [...persons]
     if (persons.find(element => element.name === newName )) {
       alert(`${newName} is already added to phonebook`)
     } 
     else {
-      copy.push({name: newName})
+      copy.push({
+        name: newName,
+        number: newNumber
+      })
       setPersons(copy)
     }
   }
@@ -32,11 +36,18 @@ const App = () => {
             />
         </div>
         <div>
+          number: <input
+            type="text"
+            value={newNumber}
+            onChange={(e)=>setNewNumber(e.target.value)}
+            />
+        </div>
+        <div>
           <button type="submit" onClick={handleSubmit}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person => (<p key={person.name}>{person.name}</p>))}
+        {persons.map(person => (<p key={person.name}>{person.name} {person.number}</p>))}
     </div>
   )
 }
