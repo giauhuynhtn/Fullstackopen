c61f16dd79d706993072c9249b8bd82c6f126a9a
Giau Huynh <giau.huynh@tuni.fi>
2022-04-12 10:12:29 +0300

    part2 2.13* Data for countries, step2

--- part2/part2c/src/App.js
+++ part2/part2c/src/App.js
@@ -1,29 +1,34 @@
-// ex 2.12* Data for countries, step1
+// ex 2.13* Data for countries, step1
 
 import { useState, useEffect } from 'react' 
 import axios from 'axios'
-import Filter from './components/Filter'
 import ShowCountries from './components/ShowCountries'
 
 const App = () => {
-  const [filter, setFilter] = useState('')
   const [countries, setCountries] = useState([])
   
   useEffect(() => {
     console.log('effect')
     axios
       .get('https://restcountries.com/v3.1/all')
-      .then(response => {
-        console.log('promise fulfilled')
-        setCountries(response.data)
-        console.log(response.data);
-      })
+      .then(response => {setCountries(response.data)})
   }, [])
+
+  const handleFilter = (event) => {
+    event.preventDefault()
+    const filterCountries = countries.filter(country => (
+      country.name.common.toLowerCase().includes(event.target.value.toLowerCase())))
+    setCountries(filterCountries)
+  };  
+
+  console.log(countries);
 
   return (
     <div>
-      <Filter filter={filter} setFilter={setFilter}/>
-      <ShowCountries filter={filter} countries={countries}/>
+      <div>
+        find countries <input onChange={handleFilter}/>
+      </div>
+      <ShowCountries countries={countries} setCountries={setCountries}/>
     </div>
   )
 }
--- part2/part2c/src/components/ShowCountries.js
+++ part2/part2c/src/components/ShowCountries.js
@@ -1,14 +1,34 @@
-const ShowCountries = ({filter, countries}) => {
-    if (filter!=='') {
-        if (countries.filter((country)=>{return country.name.common.toLowerCase().includes(filter)}).length > 10) {
-            return <p>Too many matches, specify another filter</p>
-        } else if (countries.filter((country)=>{return country.name.common.toLowerCase().includes(filter)}).length < 10) {
-            return countries
-                        .filter((country)=>{return country.name.common.toLowerCase().includes(filter)})
-                        .map((country, index) => {return <li key={index}>{country.name.common}</li>})
+
+const ShowCountries = ({countries, setCountries}) => {
+    if (countries.length === 250) {return <p>Please input a few characters to search</p>}
+    else if (countries.length > 10) {return <p>Too many matches, specify another filter</p>
+    } else if (countries.length === 1) {
+        console.log(countries);
+        return countries.map((country) => {return (
+            <div>
+                <h1>{country.name.common}</h1>
+                <p>capital {country.capital}</p>
+                <p>area {country.area}</p>
+                <h2>languages:</h2>
+                <div>
+                    {Object.values(country.languages).map((v, i)=> {return <li key={i}>{v}</li>})}
+                </div>
+                <br/>
+                <img src={country.flags.png}/>
+            </div>)})
+    } else if (countries.length < 10) {
+        return countries.map((country, index) => {
+            return (
+                <li key={index}>
+                    {country.name.common}
+                    <button onClick={()=>{
+                    setCountries([country])}}>
+                    show {country.name.common}</button>
+                </li>
+                )
+            })
                         
-        }
-    }
+        } 
 }
 
 export default ShowCountries
